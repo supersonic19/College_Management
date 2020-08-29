cf02c16d6cf46f2ce4480ecdcb8d6c7ba785cf4f
Swarad Gat <swarad2000@gmail.com>
2020-08-30 00:03:12 +0530

    On branch master

---
+++ main/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from django.core.validators import (
+    MinValueValidator,
+    MaxValueValidator
+) 
+
+# Create your models here.
+
+
+class Teacher(models.Model):
+    t_name = models.CharField(max_length=256)
+    t_id = models.IntegerField()
+    subject = models.CharField(max_length=100)
+    gender = models.BooleanField()
+
+class Course(models.Model):
+    c_name = models.CharField(max_length=100)
+    c_id = models.CharField
+    teacher = models.ForeignKey('Teacher' , on_delete = models.CASCADE)
+
+class Student(models.Model):
+    s_name = models.CharField(max_length = 256)
+    roll_no = models.IntegerField(validators=[
+                MinValueValidator(0)] )
+    s_id = models.IntegerField()
+    course = models.ManyToManyField('Course')   # Many students can have many courses
+
+    def __str__(self):
+        return self.name
---
+++ main/views.py
@@ -0,0 +1,15 @@
+from django.views.generic import(
+    DetailView,
+    ListView,
+    UpdateView,
+    DeleteView,
+    CreateView
+)
+from main import models
+
+# Create your views here.
+
+class StudentCreate(CreateView):
+    pass
+
+
